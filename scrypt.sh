b1ebb67190d593054b4b6e1edb36d8075f3ed8d8
Hugo Fernando <huginho888@outlook.com>
2023-04-30 06:36:04 -0300

    Update scrypt.sh

--- scrypt.sh
+++ scrypt.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
-
+echo "Create directories"
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos de usuários..."
-
+echo "Create groups"
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
-
+echo "Create users"
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-
 useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
-
+echo "Change permission"
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
-
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
-
-echo "Fim....."
